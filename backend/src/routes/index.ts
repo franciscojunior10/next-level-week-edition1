5b265b6ebe26751f33bb8cdd2b49fe4501c935c9
Francisco Junior <franciscojn49@gmail.com>
2020-06-08 16:11:43 -0300

    feat: added the latest backend, front end and mobile features

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,15 +1,37 @@
 import express from 'express';
-import knex from '../database/connection';
+import { celebrate, Joi } from 'celebrate';
+
+import multer from 'multer';
+import multerConfig from '../config/multer';
 
 import ItemsController from '../controllers/ItemsController';
 import PointsController from '../controllers/PointsController';
 
 const routes = express.Router();
+const upaload = multer(multerConfig);
 
 routes.get('/items', ItemsController.index);
 
-routes.post('/points', PointsController.store);
 routes.get('/points', PointsController.index);
 routes.get('/points/:id', PointsController.show);
+
+routes.post('/points',
+    upaload.single('image'),
+    celebrate({
+        body: Joi.object().keys({
+            name: Joi.string().required(),
+            email: Joi.string().required().email(),
+            whatsapp: Joi.number().required(),
+            latitude: Joi.number().required(),
+            longitude: Joi.number().required(),
+            city: Joi.string().required(),
+            uf: Joi.string().required().max(2),
+            items: Joi.string().required(),
+        })
+    }, {
+        abortEarly: false,
+    }), 
+    PointsController.store)
+;
 
 export default routes;
--- frontend/src/pages/CreatePoint/index.tsx
+++ frontend/src/pages/CreatePoint/index.tsx
@@ -5,11 +5,14 @@ import { FiArrowLeft } from 'react-icons/fi';
 import axios from 'axios';
 import { LeafletMouseEvent } from 'leaflet';
 
+import DopZonde from '../../components/DopZone';
+
 import api from '../../services/api';
 
 import logo from '../../assets/logo.svg';
 
 import './styles.css';
+import DropZone from '../../components/DopZone';
 
 interface Item {
     id: number;
@@ -42,6 +45,7 @@ const CreatePoint: React.FC = () => {
     const [selectedCity, setSelectedCity] = useState('0');
     const [selectedPosition, setSelectedPosition] = useState<[number, number]>([0, 0]);
     const [selectedItems, setselectedItems] = useState<number[]>([]);
+    const [selectedFile, setSelectedFile] = useState<File>();
 
     const history = useHistory();
 
@@ -116,22 +120,26 @@ const CreatePoint: React.FC = () => {
 
     async function handleSubmit(event: FormEvent) {
         event.preventDefault();
-
+        
         const { name, email, whatsapp } = formData;
         const uf = selectedUf;
         const city = selectedCity;
         const [latitude, longitude] = selectedPosition;
         const items = selectedItems;
 
-        const data = {
-            name,
-            email,
-            whatsapp,
-            uf,
-            city,
-            latitude,
-            longitude,
-            items,
+        const data = new FormData();
+
+        data.append('name', name);
+        data.append('email', email);
+        data.append('whatsapp', whatsapp);
+        data.append('uf', uf);
+        data.append('city', city);
+        data.append('latitude', String(latitude));
+        data.append('longitude', String(longitude));
+        data.append('items', items.join(','));
+        
+        if (selectedFile) {
+            data.append('image', selectedFile);
         }
 
         await api.post('points', data);
@@ -154,6 +162,10 @@ const CreatePoint: React.FC = () => {
 
             <form onSubmit={handleSubmit}>
                 <h1>Cadastro do <br /> ponto de coleta</h1>
+
+                <DropZone 
+                    onFileUpload={setSelectedFile}
+                />
 
                 <fieldset>
                     <legend>
